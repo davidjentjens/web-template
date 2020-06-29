78557a61a1ccc6611d27bb86e6c5f3cf1fcbc145
David Maisonnette Jentjens <davidjentjens@gmail.com>
2020-06-29 11:08:28 -0300

    File restructuring

--- conceitos_dev/typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-import { helloWorld } from './routes';
-
-app.get('/', (req, res) => helloWorld);
-
-app.listen(3333);
--- gobarber/back-end/src/shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider.ts
+++
@@ -1,5 +0,0 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTOs';
-
-export default interface IMailTemplateProvider {
-  parse(data: IParseMailTemplateDTO): Promise<string>;
-}
